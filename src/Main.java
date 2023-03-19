b834e18f84092f630c5ece0df9c06ce25a8ef77f
miinii14 <burlyaa25565@gmail.com>
2023-03-19 13:01:57 +0100

    beginning

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        Point p1 = new Point(12, 13);
+        Point p2 = new Point(33,9);
+        Segment s = new Segment(p1, p2);
+
+        System.out.println(s.distance());
     }
 }
